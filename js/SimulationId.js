8bd348516a775644944574bba11ba9a6a2cbbc11
Cayden Reynolds <cayden@arclasr.net>
2020-11-04 14:58:21 -0500

    Schema updated for factorypage

--- js/SimulationId.js
+++
@@ -1,7 +0,0 @@
-import Schema from "../js/Schema";
-import SimulationIdSchema from "../schema/SimulationId.json";
-
-export default class SimulationId extends Schema {
-    static schema = SimulationIdSchema;
-}
-Schema.RegisterSchema.call(SimulationId);
