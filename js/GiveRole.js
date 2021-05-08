839edda42d702d61e1aa5f2a61a677f3ff49efac
austin canzano <acanzano@ycp.edu>
2021-05-08 16:55:42 -0400

    Update GiveRole.js

--- js/GiveRole.js
+++ js/GiveRole.js
@@ -6,5 +6,14 @@ import User from "../js/User.js";
 
 export default class GiveRole extends Schema {
     static schema = GiveRoleSchema;
+
+    static Validate(schemaObject) {
+    schemaObject = super.Validate(schemaObject);
+
+    if ('responses' in schemaObject) {
+        schemaObject.responses = Responses.Validate(schemaObject.responses);
+    }
+        return schemaObject;
+    }
 }
 Schema.RegisterSchema.call(GiveRole);
